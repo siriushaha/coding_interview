1c156bbf0bca4491cbb563f25d6e567e70d7d1b2
Daniel Ha <daniel.ha@spin.pm>
2021-10-23 22:07:26 -0700

    Include all external modules in deps.ts

---
+++ src/deps/deps.ts
@@ -0,0 +1,5 @@
+// To maintain all external dependencies used in deps.ts
+
+export { Command } from "https://deno.land/x/cliffy@v0.19.6/command/mod.ts";
+
+export { readJsonSync } from "https://deno.land/x/jsonfile/mod.ts";
